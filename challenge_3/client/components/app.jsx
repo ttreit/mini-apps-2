ac6babd5819a8204a48f9db412d4f2d1de9d6060
ttreit <14933277+ttreit@users.noreply.github.com>
2020-11-30 20:41:53 -0800

    Create basic React structure

---
+++ challenge_3/client/components/app.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // TODO Add STATES
+    }
+  }
+  render() {
+    return <h1>App.jsx Temp Text</h1>
+  }
+}
+
+export default App;
