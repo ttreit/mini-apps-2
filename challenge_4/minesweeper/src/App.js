4a114748da496eb5159246fe08612416485398fd
ttreit <14933277+ttreit@users.noreply.github.com>
2020-12-15 18:06:54 -0800

    Board state WIP

--- challenge_4/minesweeper/src/App.js
+++ challenge_4/minesweeper/src/App.js
@@ -1,24 +1,24 @@
 import React, {Component} from 'react';
 import './App.css';
 import Board from './components/Board.js';
-import boardContents from './components/boardContents';
-import blankBoard from './components/blankBoard';
+import gameBoard from './components/gameBoard.js';
+import blankBoard from './components/blankBoard.js';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       liveBoard: [
-        [1, 9, 9, 9, 9, 9, 9, 9, 9, 1],
-        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
-        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
-        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
-        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
-        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
-        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
-        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
-        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
-        [1, 9, 9, 9, 9, 9, 9, 9, 9, 1]
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [2, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [3, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [4, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [5, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [6, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [7, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [8, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [9, 9, 9, 9, 9, 9, 9, 9, 9, 9]
       ]
     }
 
@@ -29,17 +29,19 @@ class App extends Component {
   componentDidMount() {
     console.log('component did mount');
     this.setState({liveBoard: blankBoard})
+    // this.setState({liveBoard: })
   }
 
   handleLeftClick(event) {
     console.log('Left Click');
+    //update state with gameBoard cell
+      //get cell that was clicked on
+      //
   }
 
   handleRightClick(event) {
     console.log('Right Click');
   }
-
-
 
   render() {
     return (
---
+++ challenge_4/minesweeper/src/components/gameBoard.js
@@ -0,0 +1,25 @@
+function gameBoard() {
+  const board = [
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+  ];
+
+  return board;
+}
+
+export default gameBoard();
+
+//***Possible items to display***
+//Unclicked Tile -- 9
+//Uncliked Tile with Flag
+//Revealed Tile - Bomb
+//Revealed Tile - Blank Space
+//Revealed Tile - Number of bombs that tile touches
