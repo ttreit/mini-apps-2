5db63d5dc462dc8ceb8a679b41ec5b1fd4edd7a8
ttreit <14933277+ttreit@users.noreply.github.com>
2020-12-23 13:44:59 -0800

    Initial setup

---
+++ challenge_1/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>This is the App Component</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
