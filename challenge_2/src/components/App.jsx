0adb018027c2792d7c0b165804b5f147f62c6026
ttreit <14933277+ttreit@users.noreply.github.com>
2020-12-29 17:50:44 -0800

    Create initial app components

---
+++ challenge_2/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    //add state
+  }
+
+  render() {
+    return (
+      <div>
+        This is App.jsx
+      </div>
+    )
+  }
+}
+
+export default App;
