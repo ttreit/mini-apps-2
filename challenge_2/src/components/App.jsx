dc2aeacc860e36869cea533b4dc6f0daba839b0f
ttreit <14933277+ttreit@users.noreply.github.com>
2020-12-30 20:36:13 -0800

    Create basic chart with static data

--- challenge_2/src/components/App.jsx
+++ challenge_2/src/components/App.jsx
@@ -4,13 +4,28 @@ import Chart from './Chart.jsx';
 class App extends Component {
   constructor(props) {
     super(props);
-    //add state
+    this.state = {
+      priceData: {
+        labels: ['2020-11-30', '2020-12-01', '2020-12-02'],
+        datasets: [
+          {
+            label:'Price',
+            data: [
+              19708.0967,
+              18795.1967,
+              19226.22
+            ],
+            backgroundColor:'rgba(54, 162, 235, 0.6)'
+          }
+        ]
+      }
+    }
   }
 
   render() {
     return (
       <div>
-        <Chart />
+        <Chart priceData={this.state.priceData}/>
       </div>
     )
   }
--- challenge_2/src/components/Chart.jsx
+++ challenge_2/src/components/Chart.jsx
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import { Bar, Line, Pie } from 'react-chartjs-2';
 
 class Chart extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className='chart'>
-        CHART GOES HERE
+        <Line
+          data={this.props.priceData}
+          options={{
+            maintainAspectRatio: false
+          }}
+        />
       </div>
     )
   }
