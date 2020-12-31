8f4ed82c65504d34c0973b0c7e0bdb1399982b32
ttreit <14933277+ttreit@users.noreply.github.com>
2020-12-30 19:32:45 -0800

    Create basic route for getting price data

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const port = 7100;
 
@@ -8,6 +9,22 @@ app.get('/', (req, res) => {
   res.sendFile(index.html)
 })
 
+app.get('/api/thirty', (req, res) => {
+  axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+  .then((thirtyDayData) => {
+    console.log('30', thirtyDayData.data.bpi)
+    res.json(thirtyDayData.data.bpi)
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+})
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
+
+
+
+
+
